cc42eb312c8762c50bb494600cfe59f850939504
gonza <gonza56d@gmail.com>
2021-07-09 18:40:29 -0300

    signup form

--- hitmeapp/index/views/index.py
+++ hitmeapp/index/views/index.py
@@ -3,7 +3,10 @@
 # Django
 from django.shortcuts import render
 
+# Project
+from hitmeapp.users.forms import SignupForm
+
 
 def index(request):
-    print('f')
-    return render(request, 'index/main.html', {})
+    signup_form = SignupForm(prefix='signup')
+    return render(request, 'index/main.html', {'signup_form': signup_form})
--- hitmeapp/users/forms.py
+++ hitmeapp/users/forms.py
@@ -2,15 +2,47 @@
 
 # Django
 from django import forms
-from django.forms.widgets import EmailInput, PasswordInput, TextInput
+from django.forms.widgets import EmailInput, NumberInput, PasswordInput, TextInput
+from django.utils.translation import gettext as _
 
 
 class LoginForm(forms.Form):
 
-    username = forms.CharField(
-        widget=EmailInput(attrs={'class': 'form-control'})
+    email = forms.CharField(widget=EmailInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Email'
+        })
     )
 
-    password = forms.CharField(
-        widget=PasswordInput(attrs={'class': 'form-control', 'type': 'password'})
+    password = forms.CharField(widget=PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'type': 'password',
+            'placeholder': 'Password'
+        })
     )
+
+
+class SignupForm(LoginForm):
+
+    first_name = forms.CharField(widget=TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': _('First name')
+        }
+    ))
+
+    last_name = forms.CharField(widget=TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': _('Last name')
+        }
+    ))
+
+    phone_number = forms.CharField(widget=NumberInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': _('Phone number')
+        }
+    ))
