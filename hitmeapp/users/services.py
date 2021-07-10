4e0791b229a364a6cceb3949a3d0eea8ce9298dc
gonza <gonza56d@gmail.com>
2021-07-10 01:48:59 -0300

    User signup view with class based view

---
+++ hitmeapp/users/services.py
@@ -0,0 +1,29 @@
+"""Users business logic."""
+
+# Project
+from .models import User
+
+
+def create_user(email: str, password: str, first_name: str, last_name: str,
+                phone_number: int) -> User:
+    """Handle logic to create a new user and profile.
+
+    Parameters
+    ----------
+    email : str
+        User email. It must be unique, creation won't success otherwise.
+
+    password : str
+        User password. It must contain at least 8 characters.
+
+    first_name : str
+        User first name.
+    
+    last_name : str
+        User last name.
+
+    phone_number : int
+        User phone number. Not required. Used for option to receive notifications
+        via WhatsApp.
+    """
+    pass
--- hitmeapp/users/views.py
+++ hitmeapp/users/views.py
@@ -1,5 +1,37 @@
 """Users views."""
 
+# Django
+from django.contrib import messages
+from django.http.response import HttpResponse
+from django.views.generic.edit import FormView
+from django.urls import reverse
+from django.utils.translation import gettext as _
+
+# Project
+from .forms import SignupForm
+from . import services
+
 
 def signup(request):
     print('YEA')
+
+
+class SignupView(FormView):
+
+    template_name = 'index/main.html'
+    form_class = SignupForm
+    success_url = reverse('users:signup')
+
+    def form_valid(self, form) -> HttpResponse:
+        user = services.create_user(
+            email=form.cleaned_data.get('email'),
+            password=form.cleaned_data.get('password'),
+            first_name=form.cleaned_data.get('first_name'),
+            last_name=form.cleaned_data.get('last_name'),
+            phone_number=form.cleaned_data.get('phone_number')
+        )
+        if user is not None:
+            messages.success(self.request, _('Account created successfully'))
+        else:
+            messages.error(self.request, _('An error occurred'))
+        return super().form_valid(form)
