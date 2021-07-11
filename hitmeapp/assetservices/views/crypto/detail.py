095f4b557765e5a29c1625d106d9bf5016934f77
gonza <gonza56d@gmail.com>
2021-07-11 19:27:29 -0300

    crypto detail

--- hitmeapp/assetservices/views/crypto/detail.py
+++ hitmeapp/assetservices/views/crypto/detail.py
@@ -23,21 +23,25 @@ class CryptoDetailView(DetailView):
     template_name = 'crypto/detail.html'
 
     def build_asset(self) -> CryptoCurrency:
+        symbol = self.header.find('small', {'class': 'nameSymbol___1arQV'}).text
+        self.header.find('small', {'class': 'nameSymbol___1arQV'}).decompose()
         return CryptoCurrency(
-            name=self.header.xpath('*//div/div[1]/div[1]/h2'),
-            price='test',
-            last_day='',
+            number=self.header.find('div', {'class': 'namePill___3p_Ii namePillPrimary___2-GWA'}).text,
+            name=self.header.find('h2', {'class': 'sc-1q9q90x-0 iYFMbU h1___3QSYG'}).text + ' (' + symbol + ')',
+            price=self.header.find('div', {'class': 'priceValue___11gHJ'}).text,
+            last_day=self.detail.find_all('span', {'class': 'sc-15yy2pl-0 kAXKAX'})[0].text,
             last_week='',
-            market_cap='',
-            volume='',
-            circulating_supply=''
+            market_cap=self.header.find_all('div', {'class': 'statsValue___2iaoZ'})[0].text,
+            volume=self.header.find_all('div', {'class': 'statsValue___2iaoZ'})[2].text,
+            circulating_supply=self.header.find_all('div', {'class': 'statsValue___2iaoZ'})[4].text
         )
 
     def set_soup(self) -> None:
         curl = requests.get('https://coinmarketcap.com/currencies/' + self.currency)
         self.soup = BeautifulSoup(curl.text, 'html.parser')
-        self.header = self.soup.find_all('div', {'class': 'sc-16r8icm-0'})[3]
+        self.header = self.soup.find('div', {'class': 'sc-16r8icm-0 dOJIkS container___lbFzk'})
         self.detail = self.soup.find('div', {'class': 'sc-16r8icm-0 nds9rn-0 cPoSGb'})
+        print(self.header)
 
     def get_object(self, queryset=None) -> CryptoCurrency:
         self.set_soup()
