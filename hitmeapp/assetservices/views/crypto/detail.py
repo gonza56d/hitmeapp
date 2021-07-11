a16636e78c694fff82e159eaf517b5788f90722a
gonza <gonza56d@gmail.com>
2021-07-11 18:41:44 -0300

    remove horrible lxml dependencies

--- hitmeapp/assetservices/views/crypto/detail.py
+++ hitmeapp/assetservices/views/crypto/detail.py
@@ -1,6 +1,7 @@
 """Views for detailing crypto currency asset services."""
 
 # Python
+from io import StringIO
 from typing import Any
 import requests
 
@@ -9,8 +10,9 @@ from django.views.generic.detail import DetailView
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 
-# BS4
+# BS4 and lxml
 from bs4 import BeautifulSoup
+from lxml import etree
 
 # Project
 from hitmeapp.assetservices.models import CryptoCurrency
@@ -21,9 +23,8 @@ class CryptoDetailView(DetailView):
     template_name = 'crypto/detail.html'
 
     def build_asset(self) -> CryptoCurrency:
-        print('build_asset')
         return CryptoCurrency(
-            name='test',
+            name=self.header.xpath('*//div/div[1]/div[1]/h2'),
             price='test',
             last_day='',
             last_week='',
@@ -36,7 +37,7 @@ class CryptoDetailView(DetailView):
         curl = requests.get('https://coinmarketcap.com/currencies/' + self.currency)
         self.soup = BeautifulSoup(curl.text, 'html.parser')
         self.header = self.soup.find_all('div', {'class': 'sc-16r8icm-0'})[3]
-        self.detail = self.sound.find('div', {'class': 'sc-16r8icm-0 nds9rn-0 cPoSGb'})
+        self.detail = self.soup.find('div', {'class': 'sc-16r8icm-0 nds9rn-0 cPoSGb'})
 
     def get_object(self, queryset=None) -> CryptoCurrency:
         self.set_soup()
