d5228a26b6b1638cea3f9a7522f7a264c8be73fc
Gonza Garcia <gonzalo.garcia01@globant.com>
2021-07-14 20:00:50 -0300

    detailfixes;

--- hitmeapp/assetservices/views/crypto/detail.py
+++ hitmeapp/assetservices/views/crypto/detail.py
@@ -26,9 +26,9 @@ class CryptoDetailView(DetailView):
         self.header.find('small', {'class': 'nameSymbol___1arQV'}).decompose()
         return CryptoCurrency(
             number=self.header.find('div', {'class': 'namePill___3p_Ii namePillPrimary___2-GWA'}).text,
-            name=self.header.find('h2', {'class': 'sc-1q9q90x-0 iYFMbU h1___3QSYG'}).text + ' (' + symbol + ')',
+            name=self.header.find('h2', {'class': 'sc-1q9q90x-0'}).text + ' (' + symbol + ')',
             price=self.header.find('div', {'class': 'priceValue___11gHJ'}).text,
-            last_day=self.detail.find_all('span', {'class': 'sc-15yy2pl-0 kAXKAX'})[0].text,
+            last_day=None,
             last_week='',
             market_cap=self.header.find_all('div', {'class': 'statsValue___2iaoZ'})[0].text,
             volume=self.header.find_all('div', {'class': 'statsValue___2iaoZ'})[2].text,
@@ -38,16 +38,14 @@ class CryptoDetailView(DetailView):
     def set_soup(self) -> None:
         curl = requests.get('https://coinmarketcap.com/currencies/' + self.currency)
         self.soup = BeautifulSoup(curl.text, 'html.parser')
-        self.header = self.soup.find('div', {'class': 'sc-16r8icm-0 dOJIkS container___lbFzk'})
-        self.detail = self.soup.find('div', {'class': 'sc-16r8icm-0 nds9rn-0 cPoSGb'})
-        print(self.header)
+        self.header = self.soup.find_all('div', {'class': 'container'})[3]
 
     def get_object(self, queryset=None) -> CryptoCurrency:
         self.set_soup()
         return self.build_asset()
 
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
-        self.currency = kwargs['currency']
+        self.currency = kwargs['currency'].lower()
         self.object = self.get_object()
         context = self.get_context_data()
         return self.render_to_response(context)
