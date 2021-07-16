8b208b6938423a3aa91ee6c1f09522fd6c544942
Gonza Garcia <gonzalo.garcia01@globant.com>
2021-07-16 01:36:32 -0300

    crypto list view group and limit

--- hitmeapp/assetservices/views/crypto/list.py
+++ hitmeapp/assetservices/views/crypto/list.py
@@ -5,6 +5,7 @@ from typing import Any
 
 # Django
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Count
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.views.generic.list import ListView
@@ -20,6 +21,11 @@ class CryptoListView(LoginRequiredMixin, ListView):
     external_requests = ListCryptoExternalRequest
 
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
-        self.object_list = CryptoCurrency.objects.filter(rank__lte=100)[:100]
+        currencies = CryptoCurrency.objects.annotate(occurrences=Count('name'))
+        currencies = set([c.name for c in currencies])
+        self.object_list = []
+        for currency in currencies:
+            self.object_list.append(CryptoCurrency.objects.filter(name=currency).latest())
+        self.object_list.sort(key=lambda x: x.rank)
         context = self.get_context_data()
         return self.render_to_response(context)
